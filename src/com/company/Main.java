b4247486e408be2eedba58783938882b16e6112a
ovidiu todea <ovi.todea@yahoo.com>
2020-01-24 17:17:08 +0200

    add requirement

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,19 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
+/*
+By using Scanner, find the phone number of a specific name. PhoneBook is persisted in the "phonebook.txt".
+phonebook.txt
 
+Abby 0321323132
+Jolly 0313212222
+Marius-Silviu Jilca 0313221222
+Yey Ronaldo Perez 0231332211
+Andrei Mladin 0731299212
+
+Input: Marius
+Output: 0313221222
+ */
 public class Main {
 
     public static void main(String[] args) throws IOException {
